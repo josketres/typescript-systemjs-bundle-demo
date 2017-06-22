48215e8877610cb1d327f950a7f8d0ce6ab89d13
Josue Zarzosa <josue.zarzosa@esailors.de>
2017-06-22 14:36:03 +0000

    cleanup

--- src/lib/bar.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var foo_1 = require("./foo");
-function bar() {
-    foo_1.default();
-}
-exports.default = bar;
--- src/lib/foo.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function foo() {
-    document.querySelector('body').setAttribute('style', 'background-color:blue;color:white;');
-}
-exports.default = foo;
