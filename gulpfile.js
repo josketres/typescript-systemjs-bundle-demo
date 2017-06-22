235747c8813c27791b2b1f886af5e83eaba878b4
Josue Zarzosa <josue.zarzosa@esailors.de>
2017-06-22 14:54:41 +0000

    First working version with sourcemaps and bundles

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,24 @@ const typescript = require('typescript');
 const webserver = require('gulp-webserver');
 const sourcemaps = require('gulp-sourcemaps');
 const Builder = require('systemjs-builder');
+const path = require('path');
 
 gulp.task('tsc', () => {
     const tsProject = ts.createProject('tsconfig.json');
-    return gulp
-        .src('src/**/*.ts')
+    return gulp.src('src/**/*.ts', {base: 'src'})
         .pipe(sourcemaps.init())
         .pipe(tsProject()).js
-        .pipe(sourcemaps.write('.', {sourceRoot: '.'}))
+        .pipe(sourcemaps.mapSources(function (sourcePath, file) {
+            return path.basename(sourcePath);
+        }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./src'));
 });
 
 gulp.task('bundle', ['tsc'], () => {
     const builder = new Builder('./src/', 'system.conf.js');
     return builder
-        .bundle('main.js', 'src/bundle-manual.js', {
+        .bundle('main.js', 'src/bundle.js', {
             sourceMaps: true,
             sourceMapContents: true
         })
